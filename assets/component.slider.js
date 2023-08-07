7bfc12939bade4a682c3ae00ddff293f41abf255
Michael Ballen <mballen@acid.cl>
2023-08-07 23:28:00 +1000

    Slider component

---
+++ assets/component.slider.js
@@ -0,0 +1,36 @@
+class Slider extends HTMLElement {
+  constructor () {
+    super()
+
+    this.loop = this.getAttribute("loop") === 'true' ? true : false;
+    this.direction = this.getAttribute("direction");
+    this.slidersPerPage = this.getAttribute("sliders-per-page") === 'auto' ? 'auto' : parseInt(this.getAttribute("sliders-per-page"));
+    this.autoplay
+    this.delay
+  }
+
+  static get observedAttributes() {
+    return [''];
+  }
+
+  connectedCallback () {
+    const swiper = new Swiper(this, {
+      direction: this.direction,
+      loop: this.loop,
+      slidesPerView: this.slidersPerPage,
+    
+      // If we need pagination
+      pagination: {
+        el: this.querySelector('.swiper-pagination'),
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: this.querySelector('.swiper-button-next'),
+        prevEl: this.querySelector('.swiper-button-prev'),
+      }
+    });
+  }
+}
+
+customElements.define("carousel-slider", Slider);
